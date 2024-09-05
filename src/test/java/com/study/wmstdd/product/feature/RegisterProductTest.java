627786ccfcfe4434a27c3b0ff0b2aa7bd1d2970e
이재민 <rkwoals524@hanmail.net>
2024-09-05 23:40:46 +0900

    8. create RegisterProductApi

--- src/test/java/com/study/wmstdd/product/feature/RegisterProductTest.java
+++ src/test/java/com/study/wmstdd/product/feature/RegisterProductTest.java
@@ -1,17 +1,12 @@
 package com.study.wmstdd.product.feature;
 
 import com.study.wmstdd.product.common.ApiTest;
-import com.study.wmstdd.product.domain.Category;
 import com.study.wmstdd.product.domain.ProductRepository;
-import com.study.wmstdd.product.domain.TemperatureZone;
-import com.study.wmstdd.product.feature.RegisterProduct.Request;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import com.study.wmstdd.product.feature.api.RegisterProductApi;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 class RegisterProductTest extends ApiTest {
     @Autowired
@@ -20,40 +15,7 @@ class RegisterProductTest extends ApiTest {
     @DisplayName("상품을 등록한다.")
     @Test
     void registerProduct() {
-        // given
-        final String name = "name";
-        String code = "code";
-        final String description = "description";
-        final String brand = "brand";
-        final String maker = "maker";
-        final String origin = "origin";
-        final Long weightInGrams = 100L;
-        final Long widthInMillimeters = 100L;
-        final Long heightInMillimeters = 100L;
-        final Long lengthInMillimeters = 100L;
-        RegisterProduct.Request request = new Request(
-            name,
-            code,
-            description,
-            brand,
-            maker,
-            origin,
-            Category.ELECTRONICS,
-            TemperatureZone.ROOM_TEMPERATURE,
-            weightInGrams,
-            widthInMillimeters,
-            heightInMillimeters,
-            lengthInMillimeters
-        );
-
-        // when
-        RestAssured.given().log().all()
-            .contentType(ContentType.JSON)
-            .body(request)
-            .when()
-            .post("/products")
-            .then().log().all()
-            .statusCode(HttpStatus.CREATED.value());
+        new RegisterProductApi().request();
 
         // then
         Assertions.assertThat(productRepository.findAll()).hasSize(1);
---
+++ src/test/java/com/study/wmstdd/product/feature/api/RegisterProductApi.java
@@ -0,0 +1,54 @@
+package com.study.wmstdd.product.feature.api;
+
+import com.study.wmstdd.product.domain.Category;
+import com.study.wmstdd.product.domain.TemperatureZone;
+import com.study.wmstdd.product.feature.RegisterProduct.Request;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.springframework.http.HttpStatus;
+
+public class RegisterProductApi {
+
+    private String name = "name";
+    private String code = "code";
+    private String description = "description";
+    private String brand = "brand";
+    private String maker = "maker";
+    private String origin = "origin";
+    private Long weightInGrams = 100L;
+    private Long widthInMillimeters = 100L;
+    private Long heightInMillimeters = 100L;
+    private Long lengthInMillimeters = 100L;
+
+    public RegisterProductApi name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void request() {
+        // given
+        Request request = new Request(
+            name,
+            code,
+            description,
+            brand,
+            maker,
+            origin,
+            Category.ELECTRONICS,
+            TemperatureZone.ROOM_TEMPERATURE,
+            weightInGrams,
+            widthInMillimeters,
+            heightInMillimeters,
+            lengthInMillimeters
+        );
+
+        // when
+        RestAssured.given().log().all()
+            .contentType(ContentType.JSON)
+            .body(request)
+            .when()
+            .post("/products")
+            .then().log().all()
+            .statusCode(HttpStatus.CREATED.value());
+    }
+}
