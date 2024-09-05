3178ebd697c3a23ca0b628c103a5ca2c14a7958c
이재민 <rkwoals524@hanmail.net>
2024-09-05 22:18:49 +0900

    2. create test case registerProduct

--- src/test/java/com/study/wmstdd/product/feature/RegisterProductTest.java
+++ src/test/java/com/study/wmstdd/product/feature/RegisterProductTest.java
@@ -1,5 +1,17 @@
 package com.study.wmstdd.product.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterProductTest {
 
+    @DisplayName("상품을 등록한다.")
+    @Test
+    void registerProduct() {
+        // given
+
+        // when
+
+        // then
+    }
 }
