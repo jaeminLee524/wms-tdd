5ffa53f7fa4c8b7cb14aa918e9d513fba11c9355
이재민 <rkwoals524@hanmail.net>
2024-09-06 00:11:21 +0900

    10. product jpa entity로 변경

--- src/main/java/com/study/wmstdd/product/domain/Product.java
+++ src/main/java/com/study/wmstdd/product/domain/Product.java
@@ -1,18 +1,62 @@
 package com.study.wmstdd.product.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@Entity
+@Table(name = "product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Comment("상품")
 public class Product {
 
-    private final String name;
-    private final String code;
-    private final String description;
-    private final String brand;
-    private final String maker;
-    private final String origin;
-    private final Category electronics;
-    private final TemperatureZone temperatureZone;
-    private final Long weightInGrams;
-    private final ProductSize productSize;
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_no")
+    @Comment("상품 번호")
+    private Long productNo;
+    @Column(name = "name", nullable = false)
+    @Comment("상품명")
+    private String name;
+    @Column(name = "code", nullable = false, unique = true)
+    @Comment("상품코드")
+    private String code;
+    @Column(name = "description", nullable = false)
+    @Comment("상품설명")
+    private String description;
+    @Column(name = "brand", nullable = false)
+    @Comment("브랜드")
+    private String brand;
+    @Column(name = "maker", nullable = false)
+    @Comment("제조사")
+    private String maker;
+    @Column(name = "origin", nullable = false)
+    @Comment("원산지")
+    private String origin;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    @Comment("카테고리")
+    private Category category;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "temperature_zone", nullable = false)
+    @Comment("온도대")
+    private TemperatureZone temperatureZone;
+    @Column(name = "weight_in_grams", nullable = false)
+    @Comment("무게(그램)")
+    private Long weightInGrams;
+    @Embedded
+    private ProductSize productSize;
 
     public Product(
         String name,
@@ -21,7 +65,7 @@ public class Product {
         String brand,
         String maker,
         String origin,
-        Category electronics,
+        Category category,
         TemperatureZone temperatureZone,
         Long weightInGrams,
         ProductSize productSize) {
@@ -32,17 +76,9 @@ public class Product {
         this.brand = brand;
         this.maker = maker;
         this.origin = origin;
-        this.electronics = electronics;
+        this.category = category;
         this.temperatureZone = temperatureZone;
         this.weightInGrams = weightInGrams;
         this.productSize = productSize;
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return this.id;
     }
 }
--- src/main/java/com/study/wmstdd/product/domain/ProductRepository.java
+++ src/main/java/com/study/wmstdd/product/domain/ProductRepository.java
@@ -1,23 +1,6 @@
 package com.study.wmstdd.product.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Component
-public class ProductRepository {
-
-    private final Map<Long, Product> map = new HashMap<>();
-    private Long nextId = 1L;
-
-    public void save(Product product) {
-        product.assignId(nextId++);
-        map.put(product.getId(), product);
-    }
-
-    public List<Product> findAll() {
-        return new ArrayList<>(map.values());
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
