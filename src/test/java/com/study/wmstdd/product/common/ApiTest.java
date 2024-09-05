834542a0897642cd39b9a2d853b93cccac2689ad
이재민 <rkwoals524@hanmail.net>
2024-09-06 00:20:02 +0900

    12. DatabaseCleaner 추가

--- src/test/java/com/study/wmstdd/product/common/ApiTest.java
+++ src/test/java/com/study/wmstdd/product/common/ApiTest.java
@@ -2,6 +2,7 @@ package com.study.wmstdd.product.common;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -12,10 +13,14 @@ public class ApiTest {
     @LocalServerPort
     private int post;
 
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
     @BeforeEach
     void setUp() {
         if (RestAssured.UNDEFINED_PORT == RestAssured.port) {
             RestAssured.port = post;
         }
+        databaseCleaner.execute();
     }
 }
