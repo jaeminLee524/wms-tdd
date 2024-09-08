16d689411401d6fd92f42178ab6e1e3a748e4ad0
이재민 <rkwoals524@hanmail.net>
2024-09-08 23:56:26 +0900

    10. Inbound JPA 변환

--- src/main/java/com/study/wmstdd/inbound/domain/InboundItem.java
+++ src/main/java/com/study/wmstdd/inbound/domain/InboundItem.java
@@ -1,14 +1,47 @@
 package com.study.wmstdd.inbound.domain;
 
 import com.study.wmstdd.product.domain.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "inbound_item")
+@Comment("입고 상품")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InboundItem {
 
-    private final Product product;
-    private final Long quantity;
-    private final Long unitPrice;
-    private final String description;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("입고 상품 번호")
+    private Long inboundItemNo;
+    @Comment("상품")
+    @JoinColumn(name = "product_no", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+    @Comment("수량")
+    @Column(name = "quantity", nullable = false)
+    private Long quantity;
+    @Comment("단가")
+    @Column(name = "unit_price", nullable = false)
+    private Long unitPrice;
+    @Comment("상품 설명")
+    @Column(name = "description", nullable = false)
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inbound_no", nullable = false)
+    @Comment("입고 번호")
+    private Inbound inbound;
 
     public InboundItem(
         Product product,
@@ -37,5 +70,9 @@ public class InboundItem {
         }
         Assert.notNull(unitPrice, "단가는 필수입니다.");
         Assert.hasText(description, "상품 설명은 필수입니다.");
+    }
+
+    public void assignInbound(Inbound inbound) {
+        this.inbound = inbound;
     }
 }
--- src/main/java/com/study/wmstdd/inbound/domain/InboundRepository.java
+++ src/main/java/com/study/wmstdd/inbound/domain/InboundRepository.java
@@ -1,23 +1,6 @@
 package com.study.wmstdd.inbound.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Component
-public class InboundRepository {
-
-    private final Map<Long, Inbound> inbounds = new HashMap<>();
-    private Long sequence = 1L;
-
-    public void save(Inbound inbound) {
-        inbound.assignId(sequence);
-        inbounds.put(sequence++, inbound);
-    }
-
-    public List<Inbound> findAll() {
-        return new ArrayList<>(inbounds.values());
-    }
+public interface InboundRepository extends JpaRepository<Inbound, Long> {
 }
