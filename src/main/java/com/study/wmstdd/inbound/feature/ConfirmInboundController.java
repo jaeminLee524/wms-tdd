bf314c9b3692f6289a05b2e050e29a67a9ce115c
이재민 <rkwoals524@hanmail.net>
2024-09-09 22:58:10 +0900

    8. confirmInbound API

--- src/main/java/com/study/wmstdd/inbound/feature/ConfirmInboundController.java
+++ src/main/java/com/study/wmstdd/inbound/feature/ConfirmInboundController.java
@@ -2,16 +2,21 @@ package com.study.wmstdd.inbound.feature;
 
 import com.study.wmstdd.inbound.domain.Inbound;
 import com.study.wmstdd.inbound.domain.InboundRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@RestController
 class ConfirmInboundController {
 
     private final InboundRepository inboundRepository;
 
-    public ConfirmInboundController(InboundRepository inboundRepository) {
-        this.inboundRepository = inboundRepository;
-    }
-
-    public void request(Long inboundNo) {
+    @Transactional
+    @PostMapping("/inbounds/{inboundNo}/confirm")
+    public void request(@PathVariable("inboundNo") Long inboundNo) {
         Inbound inbound = inboundRepository.getBy(inboundNo);
 
         inbound.confirmed();
--- src/test/java/com/study/wmstdd/inbound/feature/ConfirmInboundControllerTest.java
+++ src/test/java/com/study/wmstdd/inbound/feature/ConfirmInboundControllerTest.java
@@ -2,39 +2,41 @@ package com.study.wmstdd.inbound.feature;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.study.wmstdd.common.ApiTest;
+import com.study.wmstdd.common.Scenario;
 import com.study.wmstdd.inbound.domain.Inbound;
-import com.study.wmstdd.inbound.domain.InboundFixture;
 import com.study.wmstdd.inbound.domain.InboundRepository;
 import com.study.wmstdd.inbound.domain.InboundStatus;
-import org.junit.jupiter.api.BeforeEach;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
-public class ConfirmInboundControllerTest {
+public class ConfirmInboundControllerTest extends ApiTest {
 
-    private ConfirmInboundController confirmInboundController;
+    @Autowired
     private InboundRepository inboundRepository;
-
-    @BeforeEach
-    void setUp() {
-        inboundRepository = Mockito.mock(InboundRepository.class);
-        confirmInboundController = new ConfirmInboundController(inboundRepository);
-    }
 
     @DisplayName("입고를 승인한다.")
     @Test
     void confirmInbound() {
         // given
-        Long inboundNo = 1L;
-        Inbound inbound = InboundFixture.anInbound().build();
+        Scenario
+            .registerProduct().request()
+            .registerInbound().request();
 
-        Mockito.when(inboundRepository.getBy(inboundNo)).thenReturn(inbound);
+        Long inboundNo = 1L;
 
         // when
-        confirmInboundController.request(inboundNo);
+        RestAssured.given().log().all()
+            .when()
+            .post("/inbounds/{inboundNo}/confirm", inboundNo)
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value());
 
         // then
+        Inbound inbound = inboundRepository.getBy(inboundNo);
         assertThat(inbound.getStatus()).isEqualTo(InboundStatus.CONFIRMED);
     }
 }
